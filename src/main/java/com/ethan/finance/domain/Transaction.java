6ff5c4d751a83e9e7d9738ef610dfea571e9f305
Ethan <ethanp8663@gmail.com>
2026-02-20 10:07:16 -0800

    Migrated project to Spring Boot(Maven)

--- src/main/java/com/ethan/finance/domain/Transaction.java
+++ src/main/java/com/ethan/finance/domain/Transaction.java
@@ -1,9 +1,9 @@
-package com.ethan.finance.domain;
-
-import java.time.LocalDate;
-
-public class Transaction {
-    private final LocalDate recordAt;
-    private final Money money;
-
-}
+//package com.ethan.finance.domain;
+//
+//import java.time.LocalDate;
+//
+//public class Transaction {
+//    private final LocalDate recordAt;
+//    private final Money money;
+//
+//}
