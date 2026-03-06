10ecf271ceaf77ec0cea33c8900a70e8f10efa77
Ethan <ethanp8663@gmail.com>
2026-03-06 14:16:59 -0800

    Removed magic number and string. Wrote documentation.

--- src/main/java/com/ethan/finance/app/TransactionParser.java
+++ src/main/java/com/ethan/finance/app/TransactionParser.java
@@ -2,34 +2,49 @@ package com.ethan.finance.app;
 
 import com.ethan.finance.domain.Result;
 import com.ethan.finance.domain.Type;
+import com.ethan.finance.shared.FieldName;
+import com.ethan.finance.shared.ValidationMessage;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 final class TransactionParser {
+    private static final String ERR_MSG_RECORD_AT_FORMAT = ValidationMessage.shouldBe(FieldName.RECORD_AT, "YYYY-MM-DD");
+    private static final String ERR_MSG_TYPE = ValidationMessage.shouldBe(FieldName.TYPE, "INCOME or EXPENSE");
+
     private static final DateTimeFormatter DATE_FMT = DateTimeFormatter.ISO_LOCAL_DATE; // yyyy-MM-dd
 
-    static Result<LocalDate> parseRecordAt(final String date)
+    static Result<LocalDate> parseRecordAt(final String recordAt)
     {
+        if(recordAt == null)
+        {
+            return Result.err(ERR_MSG_RECORD_AT_FORMAT);
+        }
+
         final LocalDate parsedDate;
         final String strippedDate;
 
         try
         {
-            strippedDate = date.strip();
+            strippedDate = recordAt.strip();
             parsedDate = LocalDate.parse(strippedDate, DATE_FMT);
 
             return Result.ok(parsedDate);
         }
         catch(final DateTimeParseException e)
         {
-            return Result.err("Date should conform to yyyy-MM-dd.");
+            return Result.err(ERR_MSG_RECORD_AT_FORMAT);
         }
     }
 
     static Result<Type> parseType(final String type)
     {
+        if(type == null)
+        {
+            return Result.err(ERR_MSG_TYPE);
+        }
+
         final String normalized;
 
         normalized = type.strip().toUpperCase();
@@ -37,7 +52,7 @@ final class TransactionParser {
         {
             case "INCOME" -> Result.ok(Type.INCOME);
             case "EXPENSE" -> Result.ok(Type.EXPENSE);
-            default -> Result.err("Type must be INCOME or EXPENSE.");
+            default -> Result.err(ERR_MSG_TYPE);
         };
     }
 }
--- src/main/java/com/ethan/finance/app/TransactionService.java
+++ src/main/java/com/ethan/finance/app/TransactionService.java
@@ -1,13 +1,27 @@
 package com.ethan.finance.app;
 
 import com.ethan.finance.domain.*;
+import com.ethan.finance.shared.FieldName;
+import com.ethan.finance.shared.ValidationMessage;
 
 import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
+/***
+ * Creates a transaction and validates user input that needs dependencies.
+ */
 public final class TransactionService {
+    private static final String NULL_MSG_T_REPO = ValidationMessage.mustNotBeNull("Transaction repository");
+    private static final String NULL_MSG_C_REPO = ValidationMessage.mustNotBeNull("Category repository");
+    private static final String NULL_MSG_CLOCK = ValidationMessage.mustNotBeNull("Clock");
+    private static final String NULL_MSG_DRAFT = ValidationMessage.mustNotBeNull("Transaction draft");
+
+    private static final String ERR_MSG_RECORD_AT_NULL = ValidationMessage.cannotBe(FieldName.RECORD_AT, "empty");
+    private static final String ERR_MSG_DATE_FUTURE = ValidationMessage.cannotBe(FieldName.RECORD_AT, "in the future");
+    private static final String ERR_MSG_CATEGORY_INVALID = ValidationMessage.shouldBe(FieldName.CATEGORY, "valid");
+
     private final TransactionRepository transactionRepository;
     private final CategoryRepository categoryRepository;
     private final Clock clock;
@@ -16,18 +30,25 @@ public final class TransactionService {
                               final CategoryRepository categoryRepository,
                               final Clock clock)
     {
-        Objects.requireNonNull(transactionRepository, "Transaction repository can not be null.");
-        Objects.requireNonNull(categoryRepository, "Category repository can not be null.");
-        Objects.requireNonNull(clock, "Clock can not be null.");
+        Objects.requireNonNull(transactionRepository, NULL_MSG_T_REPO);
+        Objects.requireNonNull(categoryRepository, NULL_MSG_C_REPO);
+        Objects.requireNonNull(clock, NULL_MSG_CLOCK);
 
         this.transactionRepository = transactionRepository;
         this.categoryRepository = categoryRepository;
         this.clock = clock;
     }
 
+    /***
+     * Creates a transaction.
+     * Does not throw exceptions for user input mistake.
+     *
+     * @param transactionDraft the draft
+     * @return transaction id
+     */
     public Result<Long> create(final TransactionDraft transactionDraft)
     {
-        Objects.requireNonNull(transactionDraft, "Transaction draft can not be null.");
+        Objects.requireNonNull(transactionDraft, NULL_MSG_DRAFT);
 
         final List<Result<?>> resultList;
         final List<String> errorList;
@@ -43,6 +64,8 @@ public final class TransactionService {
         final Transaction transaction;
         final long transactionId;
 
+        // monadic approach
+        // parse and validate. If parse fails, short circuit.
         recordAtResult = TransactionParser.parseRecordAt(transactionDraft.recordAt()).flatMap(this::validateRecordAt);
         moneyResult = Money.parse(transactionDraft.money()).flatMap(Money::create);
         typeResult = TransactionParser.parseType((transactionDraft.type()));
@@ -78,14 +101,17 @@ public final class TransactionService {
 
     private Result<LocalDate> validateRecordAt(final LocalDate recordAt)
     {
-        Objects.requireNonNull(recordAt, "Record at should be not null.");
+        if(recordAt == null)
+        {
+            return Result.err(ERR_MSG_RECORD_AT_NULL);
+        }
 
         final LocalDate today;
 
         today = LocalDate.now(clock);
         if(recordAt.isAfter(today))
         {
-            return Result.err("Record date can not be in the future.");
+            return Result.err(ERR_MSG_DATE_FUTURE);
         }
 
         return Result.ok(recordAt);
@@ -96,7 +122,7 @@ public final class TransactionService {
         if(category == null ||
         category.strip().isBlank())
         {
-            return Result.err("Category should be valid category name.");
+            return Result.err(ERR_MSG_CATEGORY_INVALID);
         }
 
         final Result<Long> categoryId;
