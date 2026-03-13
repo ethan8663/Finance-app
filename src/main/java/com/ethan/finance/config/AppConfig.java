38298a3ab4d0ef68687e47084f88273ce2c0427a
Ethan <ethanp8663@gmail.com>
2026-03-13 13:45:43 -0700

    Created config

---
+++ src/main/java/com/ethan/finance/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.ethan.finance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Clock clock()
+    {
+        return Clock.systemDefaultZone();
+    }
+}
