1227c6f23008f2e07b0b14e2d1bb5504b53e4d9f
Ethan <ethanp8663@gmail.com>
2026-03-17 11:45:19 -0700

    Created schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE transactions(
+     id BIGINT NOT NULL AUTO_INCREMENT,
+     date DATE NOT NULL,
+     amount DECIMAL(12,2) NOT NULL,
+     type VARCHAR(10) NOT NULL,
+     category_id INT NOT NULL,
+     payer VARCHAR(20) NOT NULL,
+     payee VARCHAR(20) NOT NULL,
+     note VARCHAR(100) NOT NULL,
+     recorded_at TIMESTAMP NOT NULL,
+     PRIMARY KEY (id)
+     CONSTRAINT fk_transactions_category
+        FOREIGN KEY (category_id) REFERENCES categories(id)
+)
